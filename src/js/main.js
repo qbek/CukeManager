23d5e919f203944f5290becb2dd345f2f91a265f
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2014-11-23 00:30:11 +0100

    Updated main.js to use ExportToCVS module

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
-define(['jquery', 'modules/DataCompile'], function ($, DataCompilation) {
+define(['jquery', 'modules/DataCompile', 'modules/ExportToCVS'], function ($, DataCompilation, ExportCVS) {
   'use strict';
 
+  var features;
 
   function startEventHandlers() {
     $('body').on('click', 'a',  function (e) {
       // e.preventDefault();
-      var cvsString = DataCompilation.getCVS();
+      var cvsString = ExportCVS.getCVS(features);
       var base64 = btoa(cvsString);
       var $a = $('a');
       $a.prop('href', 'data:text;base64,' + base64);
@@ -21,7 +22,8 @@ define(['jquery', 'modules/DataCompile'], function ($, DataCompilation) {
 	$('input').on('change', function (e) {
 		var file = e.target.files[0];
 		DataCompilation.readFile(file)
-      .done (function () {
+      .done (function (compiled) {
+        features = compiled;
         startEventHandlers();
       });
     // $a.trigger('click');
