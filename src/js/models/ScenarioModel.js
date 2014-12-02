77ef3cc31443816d1d2a31a9a6f39d6ae8b975e1
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2014-12-02 22:31:16 +0100

    Update ExportToCVS to new Feature and Scenario models

--- src/js/models/ScenarioModel.js
+++ src/js/models/ScenarioModel.js
@@ -1,34 +1,20 @@
 define([], function () {
   'use strict';
 
-  function Scenario (scnName, scnDescription, scnTags, scnSteps) {
-
-    this.name = scnName;
-    this.decription = scnDescription;
-    this.tags = scnTags;
-    this.steps = scnSteps;
+  function Scenario (name) {
+    this.name = name;
+    this.decription = null;
+    this.status = 'undefined';
+    this.tags = null;
+    this.steps = null;
     this.visible = true;
-
-    Object.defineProperty(this, "status",
-      {
-        get: function () {
-          var result;
-          //check if we have any steps defined for scenario
-          var lastStep = this.steps.length - 1;
-          if(lastStep >= 0) {
-            result = this.steps[lastStep].result.status;
-            if ( result == "Skip" ) {
-              result = "failed";
-            }
-          }
-          return result;
-        }
-      });
   }
 
+
+
   return {
-    create: function (scnName, scnDescription, scnTags, scnSteps) {
-      return new Scenario(scnName, scnDescription, scnTags, scnSteps);
+    create: function (name) {
+      return new Scenario(name);
     }
   };
 });
--- src/js/modules/ExportToCVS.js
+++ src/js/modules/ExportToCVS.js
@@ -2,15 +2,15 @@ define(function () {
   'use strict';
 
   function getScenarioCVS (scenario) {
-    var scenarioCVS = String.concat(',', scenario.name);
-    scenarioCVS += String.concat(',', scenario.status);
-    scenarioCVS += String.concat(',',  scenario.tags);
+    var scenarioCVS = String.concat(';', scenario.name);
+    scenarioCVS += String.concat(';', scenario.status);
+    scenarioCVS += String.concat(';',  scenario.tags);
     scenarioCVS += '\n';
     return scenarioCVS;
   }
 
   function getFeatureCVS (feature) {
-    var featureCVS = String.concat(feature.name, ',', ',','\n');
+    var featureCVS = String.concat(feature.name, ';', ';','\n');
     var scenarios = feature.scenarios;
 
     scenarios.forEach(function (scenario) {
