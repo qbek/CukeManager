7414713ba52c0bda35b109869fbb352fe1952756
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2015-02-08 21:11:57 +0100

    Compilation to deploy version

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,34 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    clean: {
+      deploy: ['deploy']
+    },
+
+    copy: {
+      deploy: {
+        expand: 'false',
+        cwd: 'src',
+        src: ['index.html', 'css/*', 'js/vendor/*.js'],
+        dest: 'deploy'
+      }
+    },
+
+    requirejs: {
+      deploy: {
+        options: {
+          baseUrl: 'src/js',
+          paths: {
+            'text': 'vendor/text',
+            'tmpl': '../tmpl',
+          },
+          name: 'main',
+          out: 'deploy/js/main.js',
+          optimize: 'uglify2'
+        }
+      }
+    },
+
     karma: {
       options: {
         frameworks: ['jasmine', 'requirejs'],
@@ -30,4 +58,9 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+  grunt.registerTask('deploy',['clean:deploy', 'copy:deploy', 'requirejs:deploy']);
 };
