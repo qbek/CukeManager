8d84b86ce0307a00b8273533b900a5560256eaa3
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2015-03-28 22:31:13 +0100

    ExportCVS - use of quoted text to store break lines correctly

--- src/js/modules/ExportToCVS.js
+++ src/js/modules/ExportToCVS.js
@@ -1,18 +1,24 @@
 define(function () {
   'use strict';
 
+  function _getQuoted(str) {
+    var output = '';
+    return output.concat('\"', str, '\"');
+  }
+
+
   function getScenarioCVS (scenario) {
-    var scenarioCVS = String.concat(';', scenario.name);
-    scenarioCVS += String.concat(';', scenario.status.result);
-    scenarioCVS += String.concat(';',  scenario.status.comment || '');
-    scenarioCVS += '\n';
+    var name = _getQuoted(scenario.name);
+    var result = _getQuoted(scenario.status.result);
+    var comment = _getQuoted(scenario.status.comment);
+    var scenarioCVS = ''.concat(';', name, ';', result, ';', comment, '\n');
     return scenarioCVS;
   }
 
   function getFeatureCVS (feature) {
-    var featureCVS = String.concat(feature.name, ';', ';', ';', '\n');
+    var name = _getQuoted(feature.name);
+    var featureCVS = ''.concat(name, ';', ';', ';', '\n');
     var scenarios = feature.scenarios;
-
     scenarios.forEach(function (scenario) {
       var scenarioCVS = getScenarioCVS(scenario);
       featureCVS = featureCVS.concat(scenarioCVS);
@@ -23,7 +29,7 @@ define(function () {
 
   return {
     getCVS: function (features) {
-      var cvs = 'Feature name; Scenario name; Status; Comments\n';
+      var cvs = '"Feature name";"Scenario name";"Status";"Comments"\n';
 
       features.forEach(function (feature) {
         cvs += getFeatureCVS(feature);
